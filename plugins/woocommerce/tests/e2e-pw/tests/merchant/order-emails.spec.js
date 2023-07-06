bb3569420a81d0c35e6fb6638e3072e499dfdef7
Rodel Calasagsag <rodel.calasagsag@automattic.com>
2023-07-06 17:32:06 +0800

    Cherry-pick: E2e: fix order emails (#39013)

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/order-emails.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/order-emails.spec.js
@@ -50,8 +50,8 @@ test.describe( 'Merchant > Order Action emails received', () => {
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		await api.delete( `orders/${ orderId }`, { force: true } );
-		await api.delete( `orders/${ newOrderId }`, { force: true } );
+
+		await api.post( `orders/batch`, { delete: [ orderId, newOrderId ] } );
 	} );
 
 	test( 'can receive new order email', async ( { page, baseURL } ) => {
@@ -101,12 +101,16 @@ test.describe( 'Merchant > Order Action emails received', () => {
 				customerBilling.email
 			) }`
 		);
-		await expect( page.locator( 'td.column-receiver' ) ).toContainText(
-			admin.email
-		);
-		await expect( page.locator( 'td.column-subject' ) ).toContainText(
-			`[${ storeName }]: New order #${ orderId }`
-		);
+
+		// Expect row containing the admin email and the correct subject to be listed.
+		await expect(
+			page
+				.getByRole( 'row' )
+				.filter( { hasText: admin.email } )
+				.filter( {
+					hasText: `[${ storeName }]: New order #${ orderId }`,
+				} )
+		).toBeVisible();
 	} );
 
 	test( 'can email invoice/order details to customer', async ( { page } ) => {
@@ -122,11 +126,15 @@ test.describe( 'Merchant > Order Action emails received', () => {
 				customerBilling.email
 			) }`
 		);
-		await expect( page.locator( 'td.column-receiver' ) ).toContainText(
-			customerBilling.email
-		);
-		await expect( page.locator( 'td.column-subject' ) ).toContainText(
-			`Invoice for order #${ orderId } on ${ storeName }`
-		);
+
+		// Expect row containing the billing email and the correct subject to be listed.
+		await expect(
+			page
+				.getByRole( 'row' )
+				.filter( { hasText: customerBilling.email } )
+				.filter( {
+					hasText: `Invoice for order #${ orderId } on ${ storeName }`,
+				} )
+		).toBeVisible();
 	} );
 } );
